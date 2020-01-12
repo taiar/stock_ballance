8b45a26e809687fd026f801cf6889ec3b1a3dd78
André Taiar <taiar@hotmail.com>
2020-01-12 16:25:16 -0300

    Run migrations with postgres

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-    "fmt"
-    av "github.com/cmckee-dev/go-alpha-vantage"
     "github.com/joho/godotenv"
     "github.com/taiar/stock_ballance/cmd"
-    "os"
+    "github.com/taiar/stock_ballance/database"
+    "github.com/taiar/stock_ballance/migrations"
 )
 
 func main() {
     cmd.Init()
     _ = godotenv.Load()
+    database.Init()
+    migrations.Migrate()
 
-    client := av.NewClientConnection(os.Getenv("ALPHA_VANTAGE_API_KEY"), av.NewConnection())
-    data, _ := client.StockTimeSeries(av.TimeSeriesDaily, "BIDI4.SAO")
 
-    fmt.Println(data)
+    //client := av.NewClientConnection(os.Getenv("ALPHA_VANTAGE_API_KEY"), av.NewConnection())
+    //data, _ := client.StockTimeSeries(av.TimeSeriesDaily, "BIDI4.SAO")
+
+    //fmt.Println(data)
 
     //fmt.Println(*cmd.Mode)
 }
