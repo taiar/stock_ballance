b3bfee9065674d74261bb2ecb7ec3553d3a3cea3
André Taiar <taiar@hotmail.com>
2020-01-13 17:24:47 -0300

    Improve migrations with foreing keys

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -8,10 +8,9 @@ import (
 
 func Migrate() {
     fmt.Print("Migrating...")
-    database.DBCon.AutoMigrate(
-        models.Asset{},
-        models.Quotation{},
-        models.Stock{},
-        models.Wallet{})
+    database.DBCon.AutoMigrate(models.Stock{})
+    database.DBCon.AutoMigrate(models.Quotation{}).AddForeignKey("stock_id", "stocks(id)", "CASCADE", "RESTRICT")
+    database.DBCon.AutoMigrate(models.Wallet{})
+    database.DBCon.AutoMigrate(models.Asset{}).AddForeignKey("stock_id", "stocks(id)", "CASCADE", "RESTRICT").AddForeignKey("wallet_id", "wallets(id)", "RESTRICT", "RESTRICT")
     fmt.Println(" done!")
 }
