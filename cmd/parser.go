103fc99eaa521afdd1c25c37968063e292b284ad
André Taiar <taiar@hotmail.com>
2020-01-13 18:41:33 -0300

    Populating assets

--- cmd/parser.go
+++ cmd/parser.go
@@ -14,17 +14,25 @@ var (
     Code *string
     Name *string
     Description *string
+    Amount *uint
+    StockId *uint
+    WalletID *uint
+    Value *uint
 )
 const Migrate = "migrate"
 const Server = "server"
 const Populate = "populate"
 
 func Init() {
-    Command = flag.String("mode", "server", "mode selection")
-    Entity = flag.String("entity", "", "entity for creation")
-    Code = flag.String("code", "", "entity for creation")
-    Name = flag.String("name", "", "entity for creation")
-    Description = flag.String("description", "", "entity for creation")
+    Command = flag.String("mode", "server", "mode selection [migrate|populate|server]")
+    Entity = flag.String("entity", "", "set entity name for creation")
+    Code = flag.String("code", "", "set stock code")
+    Name = flag.String("name", "", "set stock name")
+    Description = flag.String("description", "", "set wallet description")
+    Amount = flag.Uint("amount", 0, "set asset amount")
+    StockId = flag.Uint("stock_id", 0, "set asset stock id")
+    WalletID = flag.Uint("wallet_id", 0, "set asset wallet id")
+    Value = flag.Uint("value", 0, "set asset current value")
     flag.Parse()
 }
 
@@ -32,12 +40,16 @@ func PopulateHandler() {
     switch *Entity {
     case "stock":
         stock := models.Stock{Code: *Code, Name: *Name}
-        database.DBCon.Create(&stock)
-        printEntity(stock)
+        createAndPrintEntity(&stock)
+        fmt.Println(stock.ID)
     case "wallet":
         wallet := models.Wallet{Description: *Description}
-        database.DBCon.Create(&wallet)
-        printEntity(wallet)
+        createAndPrintEntity(&wallet)
+        fmt.Println(wallet.ID)
+    case "asset":
+        asset := models.Asset{ Amount: *Amount, Value:  *Value, StockID: *StockId, WalletID: *WalletID }
+        createAndPrintEntity(&asset)
+        fmt.Println(asset.ID)
     default:
         fmt.Println("Unknown entity", *Entity)
     }
@@ -47,6 +59,7 @@ func Mode() string {
     return *Command
 }
 
-func printEntity(entity interface{}) {
+func createAndPrintEntity(entity interface{}) {
+    database.DBCon.Create(entity)
     fmt.Printf("%+v\n", entity)
 }
